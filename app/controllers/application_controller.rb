d9fac42d0afd55d96c7207820bd84c0051837d8f
Gilbert Torchon <gilbert@noukod.com>
2019-01-11 19:29:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,9 +34,9 @@ class ApplicationController < Sinatra::Base
   	user = User.find_by(:username => params[:username])
 		if user
 		 redirect "/success"
-	 else
-		 redirect "/failure"
-	 end
+		else
+			 redirect "/failure"
+		end
 	end
 
 	get "/success" do
